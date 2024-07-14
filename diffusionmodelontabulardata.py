951755d948511af91b2f71d208cada8c47b4158a
Zhifeng Wu <166958489+ZhifengWu7@users.noreply.github.com>
2024-07-14 15:51:35 -0700

    Update diffusionmodelontabulardata.py

--- diffusionmodelontabulardata.py
+++ diffusionmodelontabulardata.py
@@ -132,6 +132,35 @@ def train_and_evaluate(x, logsnr, model, batch_size, num_epochs, learning_rate, 
 
     return val_loss
 
+def svd_predict_and_evaluate(x, logsnr, batch_size, rank):
+    criterion = nn.MSELoss(reduction='sum')
+    total_loss = 0
+    dataset = TensorDataset(x)
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=True)
+
+    for batch in data_loader:
+        batch_x = batch[0]
+        z, eps = noisy_channels(batch_x, t.ones((len(batch_x))) * logsnr)
+
+        for i in range(z.size(0)):
+            u, s, vt = t.linalg.svd(z[i], full_matrices=False)
+
+            num_singular_values_to_keep = rank
+
+            s_hat = s[:num_singular_values_to_keep]
+            u_hat = u[:, :num_singular_values_to_keep]
+            vt_hat = vt[:num_singular_values_to_keep, :]
+
+            z_hat = t.matmul(u_hat, t.matmul(t.diag(s_hat), vt_hat))
+
+            eps_hat = z[i] - z_hat
+
+            loss = criterion(eps_hat, eps[i])
+            total_loss += loss.item()
+
+    avg_loss = total_loss / len(dataset)
+    return avg_loss
+
 def train_and_evaluate_l1(x, logsnr, model, batch_size, num_epochs, learning_rate, patience, lambda_l1):
     """Given a specific logsnr value, train the model and return val_loss."""
     dataset = TensorDataset(x)
